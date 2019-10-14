1a31a29b440f42077e9fc2ae22ed57b4070111db
anthonykao10 <anthonykao10@gmail.com>
2019-10-14 22:25:59 +0000

    add comments to main

--- main.rb
+++ main.rb
@@ -2,9 +2,11 @@ require './Game.rb'
 require './Player.rb'
 require './Question.rb'
 
+# Create players
 p1 = Player.new("player 1")
 p2 = Player.new("player 2")
 
+# Create new game
 g1 = Game.new(p1, p2)
 g1.start_game
 g1.end_game
